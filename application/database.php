77488c0f6d110590964290ad658e7eb269ae780e
tzhe <805578494@qq.com>
2019-09-15 16:12:12 +0800

    数据库链接

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+//    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => '',
+    'database'        => 'juhui',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => '5deE8sCpiY5MbThr',
     // 端口
     'hostport'        => '',
     // 连接dsn
