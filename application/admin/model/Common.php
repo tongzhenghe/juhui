d1464ffc96f4b0c3a5ea1ffd267791c79562d5a4
tzhe <805578494@qq.com>
2019-09-16 11:31:53 +0800

    菜单生成`

---
+++ application/admin/model/Common.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/9/16
+ * Time: 11:26
+ */
+
+namespace app\admin\model;
+
+
+use app\admin\model\system\SystemAdmin;
+
+class Common extends  SystemAdmin
+{
+
+
+    public  static  function  dataExecute($model, $data, $id = null)
+    {
+        if (!empty($id)) {
+            return $model->save($data, ['id' => intval($id)]);
+        } else {
+            return $model->save($data);
+        }
+
+    }
+
+
+}
