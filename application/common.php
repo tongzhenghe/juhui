62beced9dfdd9d08b1432336d4a5beaf40819e9e
tzhe <805578494@qq.com>
2019-09-16 16:01:26 +0800

    菜单生成`

--- application/common.php
+++ application/common.php
@@ -40,3 +40,20 @@ function jsondebug( $array )
 
     exit( json_encode(['str' => $array]) );
 }
+
+function tree($data, $pid = 0, $deep = 0 )
+{
+    static $arr = [];
+    foreach ($data as $val) {
+
+        if ($val['pid'] == $pid ) {
+            $val['deep'] = $deep;
+            $val['html'] = str_repeat('<span>&nbsp;|__</span>',$deep);
+            $arr[] = $val;
+            $arr = tree($data, $val['id'], $deep+1 );
+        }
+
+    }
+    return $arr;
+
+}
