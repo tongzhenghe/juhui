2bd5ec11fe37b6fb3d4694cde65508179daa16a3
tzhe <805578494@qq.com>
2019-09-16 19:03:47 +0800

    菜单生成`

---
+++ application/admin/controller/Common.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/9/16
+ * Time: 19:00
+ */
+
+namespace app\admin\controller;
+
+
+use app\admin\model\system\SystemAdmin;
+use think\Controller;
+use think\Db;
+
+class Common extends  Controller
+{
+
+    protected static $adminInfo;
+
+    public  function  _initialize()
+    {
+        parent::_initialize();
+        self::$adminInfo  =  SystemAdmin::activeAdminInfoOrFail();
+        //if(!is_dir('./tmp/'))mkdir ('./tmp/', 0700);
+       // session_save_path('./tmp/');
+
+        //if(!SystemAdmin::hasActiveAdmin()) return $this->redirect(url('Sign/signin'));
+
+        $where = ['state' => 1, 'is_del' => 1];
+        $menu = Db::name('admin_menu')->where($where)->select();
+        $menu =tree($menu);
+
+        wl_debug($menu);
+        $this->assign('menu', $menu);
+
+    }
+
+
+
+}
