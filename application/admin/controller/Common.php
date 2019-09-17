b2975d3b94fe99f76a6a53b538aa54cab6b59c59
tzhe <805578494@qq.com>
2019-09-17 18:19:00 +0800

    新闻模块

---
+++ application/admin/controller/Common.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/9/16
+ * Time: 19:00
+ */
+
+namespace app\admin\controller;
+
+
+use app\admin\model\system\SystemAdmin;
+use think\Controller;
+use think\Db;
+
+class Common extends  Controller
+{
+
+    protected static $adminInfo;
+
+    public  function  _initialize()
+    {
+        // parent::_initialize();
+        // self::$adminInfo  =  SystemAdmin::activeAdminInfoOrFail();
+        //if(!is_dir('./tmp/'))mkdir ('./tmp/', 0700);
+        // session_save_path('./tmp/');
+        //if(!SystemAdmin::hasActiveAdmin()) return $this->redirect(url('Sign/signin'));
+        $where = ['status' => 1, 'is_del' => 1];
+        $menu = Db::name('menu')->where($where)->select();
+        $menu = _reSort($menu);
+        $this->assign('menu', $menu);
+
+    }
+
+
+
+}
