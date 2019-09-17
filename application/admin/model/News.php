9d0cc0287bcefa7b92776d88039a52661a0eec19
tzhe <805578494@qq.com>
2019-09-17 11:24:37 +0800

    菜单生成`

---
+++ application/admin/model/News.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/9/17
+ * Time: 11:22
+ */
+
+namespace app\admin\model;
+
+
+use app\admin\model\system\SystemAdmin;
+
+class News extends SystemAdmin
+{
+    protected $autoWriteTimestamp = true;
+    protected $insert = ['is_del' => 1, 'status' => 1];
+
+    protected function setIpAttr()
+    {
+        return request()->ip();
+    }
+
+}
