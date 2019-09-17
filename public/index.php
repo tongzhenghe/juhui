136a18411b98f7a9d2e5ebaa2c11d2a368ac8d30
tzhe <805578494@qq.com>
2019-09-17 18:46:01 +0800

    测试七牛云

--- public/index.php
+++ public/index.php
@@ -9,13 +9,12 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-echo 342;exit;
 // [ 应用入口文件 ]
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 define('BIND_MODULE', 'admin/juhuiadmin');
 
-require_once  __DIR__ . '/src/Qiniu/functions.php';
-
+$ss = require_once  __DIR__ . '/../vendor/teg1c/thinkphp-qiniu-sdk/qiniu_php_driver/src/Qiniu/functions.php';
+wl_debug($ss);
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
