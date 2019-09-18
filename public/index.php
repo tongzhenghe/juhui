34461012b769d366f4781f1082eb6d4e6e6a885c
tzhe <805578494@qq.com>
2019-09-18 09:50:09 +0800

    测试七牛云

--- public/index.php
+++ public/index.php
@@ -9,6 +9,22 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 // [ 应用入口文件 ]
+
+
+function classLoader($class)
+{
+    $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    $file = __DIR__ . '/Qiniu/' . $path . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
+spl_autoload_register('classLoader');
+
+require_once  __DIR__ . '/../vendor/qiniu/php/src/Qiniu/functions.php';
+
+
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 define('BIND_MODULE', 'admin/juhuiadmin');
