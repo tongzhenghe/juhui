7d11ac006941b3af369f72e809fdc8057940e665
tzhe <805578494@qq.com>
2019-09-18 09:58:34 +0800

    测试七牛云

--- public/index.php
+++ public/index.php
@@ -13,5 +13,7 @@
 define('APP_PATH', __DIR__ . '/../application/');
 define('BIND_MODULE', 'admin/juhuiadmin');
 
+require_once  __DIR__.'/../vendor/autoload.php';
+
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
