d29463ef9a2d49032ed1c15ce29ecb738738fd4a
tzhe <805578494@qq.com>
2019-09-18 18:58:49 +0800

    测试七牛云

--- public/static/juhuiadmin/js/ajaxSub.js
+++ public/static/juhuiadmin/js/ajaxSub.js
@@ -1,7 +1,7 @@
 
 //config的设置是全局的
 layui.config({
-    base: '/static/juhuiadmin/js' //假设这是你存放拓展模块的根目录
+    base: '/static/juhuiadmin/js/' //假设这是你存放拓展模块的根目录
 }).extend({ //设定模块别名
     mymod: 'ajaxSub' //如果 mymod.js 是在根目录，也可以不用设定别名
     ,mod1: '/juhuiadmin/js' //相对于上述 base 目录的子目录
