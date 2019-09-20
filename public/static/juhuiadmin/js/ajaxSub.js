08f637bc67396eff200a7a5318b829a232676dac
tzhe <805578494@qq.com>
2019-09-20 09:43:54 +0800

    测试七牛云

--- public/static/juhuiadmin/js/ajaxSub.js
+++ public/static/juhuiadmin/js/ajaxSub.js
@@ -8,8 +8,24 @@ layui.config({
    // ,mod1: 'admin/mod1'  //相对于上述 base 目录的子目
 });
 
-//使用拓展模块
 
+
+layui.define(function(exports){ //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
+    var obj = {
+        hello: function(str){
+            alert('Hello '+ (str||'mymod'));
+        }
+    };
+
+    console.log(obj)
+
+
+});
+
+
+
+
+//使用拓展模块
 var laypage, laydate, layedit, upload, form;
 layui.use(['form', 'upload', 'layedit', 'laypage', 'laydate'], function(){
     laypage = layui.laypage;
