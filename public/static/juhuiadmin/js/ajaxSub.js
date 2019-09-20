0ba1354934c7bedea8271a82718d3b1a8a053b88
tzhe <805578494@qq.com>
2019-09-20 08:32:17 +0800

    测试七牛云

--- public/static/juhuiadmin/js/ajaxSub.js
+++ public/static/juhuiadmin/js/ajaxSub.js
@@ -1,25 +1,16 @@
 
-//config的设置是全局的
-layui.config({
-    base: '/static/juhuiadmin/js/' //假设这是你存放拓展模块的根目录
-})/*.extend({ //设定模块别名
-    mymod: 'ajaxSub' //如果 mymod.js 是在根目录，也可以不用设定别名
-    ,mod1: '/juhuiadmin/js' //相对于上述 base 目录的子目录
-})*/;
 
-//你也可以忽略 base 设定的根目录，直接在 extend 指定路径（主要：该功能为 layui 2.2.0 新增）
-// layui.extend({
-//     mod2: '{/}http://cdn.xxx.com/lib/mod2' // {/}的意思即代表采用自有路径，即不跟随 base 路径
-// })
+layui.define(function(exports){ //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
+    var obj = {
+        hello: function(str){
+            alert('Hello '+ (str||'mymod'));
+        }
+    };
 
-//使用拓展模块
-// layui.use(['mymod', 'mod1'], function(){
-//     var mymod = layui.mymod
-//         ,mod1 = layui.mod1
-//         ,mod2 = layui.mod2;
-//
-//     mymod.hello('World!'); //弹出 Hello World!
-// });
+    //输出test接口
+    exports('mymod', obj);
+});
+
 
 var laypage, laydate, layedit, upload, form;
 layui.use(['form', 'upload', 'layedit', 'laypage', 'laydate'], function(){
