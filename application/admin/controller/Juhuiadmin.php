7e2260523bfdb1f2e254197acc16559467d40076
tzhe <805578494@qq.com>
2019-09-20 15:05:13 +0800

    测试七牛云

--- application/admin/controller/Juhuiadmin.php
+++ application/admin/controller/Juhuiadmin.php
@@ -341,8 +341,8 @@ class Juhuiadmin extends \app\admin\controller\Common
 
         if($info){
             $name_path =str_replace('\\',"/",$info->getSaveName());
-wl_debug_log($name_path);exit;
             $result['data']["src"] = "/uploads/layui/".$name_path;
+            wl_debug_log($result);exit;
             $url    = $info->getSaveName();
             //图片上传成功后，组好json格式，返回给前端
             $arr   = array(
