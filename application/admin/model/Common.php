98b3ff433bc24de8ffb9cf93ec17aab9d75234bf
tzhe <805578494@qq.com>
2019-09-20 15:59:56 +0800

    测试七牛云

--- application/admin/model/Common.php
+++ application/admin/model/Common.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * Created by PhpStorm.
@@ -16,7 +15,7 @@ class Common extends  SystemAdmin
 {
 
 
-    public  static  function  dataExecute($model, $data, $param = null)
+    public static function dataExecute($model, $data, $param = null)
     {
 
         if (!empty($param['id'])) {
@@ -27,64 +26,17 @@ class Common extends  SystemAdmin
 
     }
 
-   public  static  function json($code, $msg = '', $url = ''){
-        $result =[
-            'code'=>$code,
-            'msg'=>$msg,
-            'url'=>$url
-            ];
-
-        return json_encode($result);
-
-    }
-
-
-
-
-
-=======
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2019/9/16
- * Time: 11:26
- */
-
-namespace app\admin\model;
-
-
-use app\admin\model\system\SystemAdmin;
-
-class Common extends  SystemAdmin
-{
-
-
-    public  static  function  dataExecute($model, $data, $param = null)
+    public static function json($code, $msg = '', $url = '')
     {
-
-        if (!empty($param['id'])) {
-            return $model->save($data, ['id' => intval($param['id'])]);
-        } else {
-            return $model->save($data);
-        }
-
-    }
-
-   public  static  function json($code, $msg = '', $url = ''){
-        $result =[
-            'code'=>$code,
-            'msg'=>$msg,
-            'url'=>$url
-            ];
+        $result = [
+            'code' => $code,
+            'msg' => $msg,
+            'url' => $url
+        ];
 
         return json_encode($result);
 
     }
 
 
-
-
-
->>>>>>> origin/master
 }
