a5efcebbc2457e8f826bb37a4f050c45b5730f7f
tzhe <805578494@qq.com>
2019-09-20 15:04:12 +0800

    测试七牛云

--- application/admin/controller/Juhuiadmin.php
+++ application/admin/controller/Juhuiadmin.php
@@ -338,10 +338,10 @@ class Juhuiadmin extends \app\admin\controller\Common
 //thinkphp5的框架，如果是原生的，用$_FiLES['file']获取到图片，
         $file   = request()->file('file');
         $info   = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
-        wl_debug_log($info);exit;
+
         if($info){
             $name_path =str_replace('\\',"/",$info->getSaveName());
-
+wl_debug_log($name_path);exit;
             $result['data']["src"] = "/uploads/layui/".$name_path;
             $url    = $info->getSaveName();
             //图片上传成功后，组好json格式，返回给前端
