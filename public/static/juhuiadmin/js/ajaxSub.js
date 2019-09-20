590656532a6c66095b53dc3987d1dcc3525569c3
tzhe <805578494@qq.com>
2019-09-20 10:00:38 +0800

    测试七牛云

--- public/static/juhuiadmin/js/ajaxSub.js
+++ public/static/juhuiadmin/js/ajaxSub.js
@@ -5,13 +5,13 @@ layui.define('jquery', function(exports){
             console.log("helloworld")
         }
     };
-    exports('base', base);
+    exports('ajaxSub', base);
 });
 layui.config({
     version: true //一般用于更新模块缓存，默认不开启。设为true即让浏览器不缓存。也可以设为一个固定的值，如：201610
     ,debug: false //用于开启调试模式，默认false，如果设为true，则JS模块的节点会保留在页面
     , base: '/static/juhuiadmin/js/' //假设这是你存放拓展模块的根目录
-}).use(['base'],function(){
+}).use(['ajaxSub'],function(){
     var base=layui.base;
     base.test()
 });
