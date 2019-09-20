f553885795a633952b6e47be7246a00aa931c57e
tzhe <805578494@qq.com>
2019-09-20 09:16:51 +0800

    测试七牛云

--- public/static/juhuiadmin/js/ajaxSub.js
+++ public/static/juhuiadmin/js/ajaxSub.js
@@ -1,21 +1,18 @@
 
-// layui.define(function(exports){ //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
-//     var obj = {
-//         hello: function(str){
-//             alert('Hello '+ (str||'ajaxSub'));
-//         }
-//     };
-//
-//     //输出test接口
-//     exports('ajaxSub', obj);
-// });
+layui.define(function(exports){ //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
+    var obj = {
+        hello: function(str){
+            alert('Hello '+ (str||'ajaxSub'));
+        }
+    };
+
+});
 
 
 
 //config的设置是全局的
-layui.config({
-    base: '/static/juhuiadmin/js/' //假设这是你存放拓展模块的根目录
-}).extend({ //设定模块别名
+layui.extend({
+
     ajaxSub: 'ajaxSub' //如果 mymod.js 是在根目录，也可以不用设定别名
    // ,mod1: 'admin/mod1'  //相对于上述 base 目录的子目录
 });
