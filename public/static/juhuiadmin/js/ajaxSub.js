16560efdcf77dfdaf6368785397a3dfa8d672fb8
tzhe <805578494@qq.com>
2019-09-20 09:23:46 +0800

    测试七牛云

--- public/static/juhuiadmin/js/ajaxSub.js
+++ public/static/juhuiadmin/js/ajaxSub.js
@@ -2,9 +2,13 @@
 layui.define(function(exports){ //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
     var obj = {
         hello: function(str){
-            alert('Hello '+ (str||'mymod'));
+            alert('Hello '+ (str||'ajaxSub'));
         }
     };
+
+    //输出test接口
+    exports('ajaxSub', obj);
+
 });
 
 
