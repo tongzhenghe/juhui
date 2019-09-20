b264ec045e011beb7e9017abba64ff53c4ba50fb
tzhe <805578494@qq.com>
2019-09-20 10:10:30 +0800

    测试七牛云

--- public/static/juhuiadmin/js/ajaxSub.js
+++ public/static/juhuiadmin/js/ajaxSub.js
@@ -1,26 +1,25 @@
 layui.define('jquery', function(exports){
     var $=layui.jquery;
-    // var base={
-    //     test:function(){
-    //         console.log("hellosssssworld")
-    //     }
-    // };
-    exports('ajaxSub', $);
+    var base={
+        test:function(){
+            console.log("hellosssssworld")
+        }
+    };
+    exports('ajaxSub', base);
 });
 layui.config({
     version: true //一般用于更新模块缓存，默认不开启。设为true即让浏览器不缓存。也可以设为一个固定的值，如：201610
     ,debug: true //用于开启调试模式，默认false，如果设为true，则JS模块的节点会保留在页面
     , base: '/static/juhuiadmin/js/' //假设这  是你存放拓展模块的根目录
-
 }).use(['ajaxSub','form', 'upload', 'layedit', 'laypage', 'laydate'],function(){
-    var laypage, laydate, layedit, upload, form;
+    var ajaxSub, laypage, laydate, layedit, upload, form;
     laypage = layui.laypage;
     laydate = layui.laydate;
+    ajaxSub=layui.ajaxSub;
     layedit = layui.layedit;
     upload = layui.upload;
     form = layui.form;
     //addmenu
-    console.log(laypage)
     form.on('submit(tos)', function(data) {
         alert(2)
     });
