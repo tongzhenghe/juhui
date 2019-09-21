1f9a343e9c2f29135c1299111bb7a9193a1501fe
tzhe <805578494@qq.com>
2019-09-21 10:54:39 +0800

    测试七牛云

---
+++ application/admin/model/Banner.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/9/21
+ * Time: 10:53
+ */
+
+namespace app\admin\model;
+
+
+use app\admin\model\system\SystemAdmin;
+
+class Banner extends  SystemAdmin
+{
+
+    protected $autoWriteTimestamp = true;
+    protected $insert = ['is_del' => 1, 'status' => 1];
+
+    protected function setIpAttr()
+    {
+        return request()->ip();
+    }
+
+
+}
