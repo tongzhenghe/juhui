d524df5b1b1ad03d24917ac921f4fcf227c59b13
tzhe <805578494@qq.com>
2019-09-23 15:30:25 +0800

    测试七牛云

--- public/static/juhuiadmin/layui/lay/kit_modules/admin.js
+++ public/static/juhuiadmin/layui/lay/kit_modules/admin.js
@@ -105,8 +105,14 @@ layui.define(mods, function (e) {
                 , {path: "/juhui/404", component: "p404", name: "code"}
                 , {path: "/juhui/500", component: "p500", name: "code"}
                 , {path: "/juhui/mockjs", component: "mockjs", name: "拦截器(Mockjs)"}
+
                 , { path: "/juhui/menu", component: "menu", name: "左侧菜单(Menu)"}
                 , { path: "/juhui/addmenu", component: "addmenu", name: "add(Menu)"}
+
+                , { path: "/juhui/umenu", component: "umenu", name: "前台菜单(umenu)"}
+                , { path: "/juhui/addumenu", component: "addumenu", name: "add(Menu)"}
+
+
                 , { path: "/juhui/menu", component: "menu", name: "左侧菜单(Menu)"}
                 , {path: "/juhui/route", component: "route", name: "路由配置(Route)"}
                 , { path: "/juhui/tabs", component: "tabs", name: "选项卡(Tabs)"}
