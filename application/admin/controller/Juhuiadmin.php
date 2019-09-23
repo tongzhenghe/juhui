46d4c6d9e9e494339eb810e1e67cd216d3791053
tzhe <805578494@qq.com>
2019-09-23 16:33:31 +0800

    测试七牛云

--- application/admin/controller/Juhuiadmin.php
+++ application/admin/controller/Juhuiadmin.php
@@ -486,7 +486,7 @@ class Juhuiadmin extends \app\admin\controller\Common
     public  function cdnUploads()
     {
         $img = $_FILES['file'];
-        wl_debug_log($img, 'logo');
+     jsondebug($img);
         $fileError = $img['error'];
         $fileType = $img['type'];
         if ($fileError == 0) {
