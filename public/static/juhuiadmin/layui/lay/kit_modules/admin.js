37b16085f78eefc26a06452ddac423f6ac071392
tzhe <805578494@qq.com>
2019-09-24 17:01:31 +0800

    产品

--- public/static/juhuiadmin/layui/lay/kit_modules/admin.js
+++ public/static/juhuiadmin/layui/lay/kit_modules/admin.js
@@ -76,6 +76,7 @@ layui.define(mods, function (e) {
                     path: "/cascader",
                     component: "views/cascader/index",
                     name: "Cascader"}
+                , {path: "/", component: "juhuiadmin.php/app", name: "主页"}
                 , {path: "/juhui/app", component: "juhuiadmin.php/app", name: "主页"}
                 , {path: "/juhui/profile", component: "juhuiadmin.php/profile", name: "用户中心"}
                 , {path: "/juhui/grid", component: "juhuiadmin.php/grid", name: "Grid"}
