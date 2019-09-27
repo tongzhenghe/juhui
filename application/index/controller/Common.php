65bbfee39a08583c4b2a94460199f50b3091aa9a
tzhe <805578494@qq.com>
2019-09-27 09:53:03 +0800

    产品

---
+++ application/index/controller/Common.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/9/27
+ * Time: 9:52
+ */
+
+namespace app\index\controller;
+
+use think\Controller;
+use think\Db;
+
+class Common extends  Controller
+{
+    public  function  _initialize()
+    {
+        $where= ['is_del' => 1, 'status' => 1, 'is_m' => 2];
+        $menu = Db::name('umenu')->where($where)->order('sort asc')->select();
+        return view('', ['menu'=> $menu]);
+    }
+
+
+
+}
