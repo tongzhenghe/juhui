9099526e7d0140c73a85e61f8d20806d4b9701fc
tzhe <805578494@qq.com>
2019-10-01 18:14:32 +0800

    文件

---
+++ application/admin/model/Webset.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/9/17
+ * Time: 11:22
+ */
+
+namespace app\admin\model;
+
+
+use app\admin\model\system\SystemAdmin;
+
+class Webset extends SystemAdmin
+{
+    protected $autoWriteTimestamp = true;
+
+    protected function setIpAttr()
+    {
+        return request()->ip();
+    }
+
+}
