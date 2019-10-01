304b171704096ed71def508d2a68fd03c32cb91d
tzhe <805578494@qq.com>
2019-10-01 15:05:31 +0800

    文件

--- application/index/controller/Common.php
+++ application/index/controller/Common.php
@@ -18,6 +18,8 @@ class Common extends  Controller
     {
         $where= ['is_del' => 1, 'status' => 1, 'is_m' => 2];
         $menu = Db::name('umenu')->where($where)->order('sort asc')->select();
+        $about = Db::name('us')->find();
+        $this->assign('about', $about);
         $this->assign('menu', $menu);
 
     }
