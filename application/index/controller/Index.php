63408a5657a47427993745084112205af0aac246
tzhe <805578494@qq.com>
2019-10-01 16:30:10 +0800

    文件

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -38,7 +38,7 @@ class Index extends IndexCommonController
     }
     public  function goodslist()
     {
-        $param = request()->get();
+        $param = request()->param('cateid');
         if (!empty($param)) {
             wl_debug($param);
         }
