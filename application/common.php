2f3dae9bbfd77ada8f955cad7f8c30715b50bfd5
tzhe <805578494@qq.com>
2019-10-01 10:58:04 +0800

    文件

--- application/common.php
+++ application/common.php
@@ -31,6 +31,26 @@ function wl_debug( $value )
 //
 //}
 
+function timeTran($time)
+{
+    $t = time() - $time;
+    $f = array(
+        '31536000' => '年',
+        '2592000' => '个月',
+        '604800' => '星期',
+        '86400' => '天',
+        '3600' => '小时',
+        '60' => '分钟',
+        '1' => '秒'
+    );
+    foreach ($f as $k => $v) {
+        if (0 != $c = floor($t / (int)$k)) {
+            return $c . $v  .'前';
+        }
+    }
+}
+
+
 function jsondebug( $array )
 {
     if (is_array($array)) {
